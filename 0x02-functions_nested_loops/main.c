a586b04ad8ce8d3fbea6a0e67f2bea9dff675c24
FOX <omniasaged111@gmail.com>
2023-03-21 15:16:39 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,41 +1,19 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
-/* _putchar.c */
 int _putchar(char);
-
-/* 1-alphabet.c */
 void print_alphabet(void);
-
-/* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
-
-/* 3-islower.c */
-int _islower(int);
-
-/* 4-alpha.c */
-int _isalpha(int);
-
-/* 5-sign.c */
-int print_sign(int);
-
-/* 6-abs.c */
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
 int _abs(int);
-
-/* 7-print_last_digit.c */
 int print_last_digit(int);
-
-/* 8-24_hours.c */
 void jack_bauer(void);
-
-/* 9-times_table.c */
 void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
 
-/* 10-add.c */
-int add(int, int);
-
-/* 11-print_to_98.c */
-void print_to_98(int);
-
-/* 100-times_table.c*/
-void print_times_table(int);
+#endif
