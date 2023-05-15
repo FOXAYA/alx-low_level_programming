002ab5ba4289113d025a167f43249fe5ef2e4252
FOX <omniasaged111@gmial.com>
2023-05-15 09:33:00 -0700

    task 03

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,17 @@
  * main - Prints the alphabet without q and e.
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-	char i;
-    	char j;
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-    	for(j='a'; j <='z'; j++)
-    {
-          putchar(j);
-    }
-     for (i ='A'; i<='Z'; i++)
-    {
-         putchar(i);
-    }
-     return (0);
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
+
