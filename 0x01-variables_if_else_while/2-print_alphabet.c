4238aeb912ded03e150fa9240217431328674132
FOX <omniasaged111@gmial.com>
2023-05-15 09:07:10 -0700

    2-print_alphabict.c correct betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	char alp_batic[26]="abcdefghijklmnopqrstuvwxyz";
-	int i ;
+{
+	char alp_batic[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for (i=0;i<26;i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp_batic[i]);
 	}
 	putchar('\n');
-	
 	return (0);
 }
